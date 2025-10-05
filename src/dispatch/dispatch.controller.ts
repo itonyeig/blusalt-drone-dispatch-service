6d0b02e237ce22144ad1bb16b994725654b41dae
itonyeig <itonyeig@gmail.com>
2025-10-06 00:15:23 +0100

    feat(dispatch): enhance DispatchController with new endpoints for loading drones and retrieving available drones
    - Added POST endpoint to load a drone with medication items, including guards for validation.
    - Introduced GET endpoints to list available drones and retrieve loaded items for a specific drone.
    - Implemented battery level check for drones with appropriate error handling.

--- src/dispatch/dispatch.controller.ts
+++ src/dispatch/dispatch.controller.ts
@@ -1,6 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Req, UseGuards } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Request } from 'express';
+import { ResponseFormatter } from 'src/common/response-formatter';
 import { DispatchService } from './dispatch.service';
+import { LoadDroneDto } from './dto/load-drone.dto';
+import { DroneByIdGuard } from './guards/drone-by-id.guard';
+import { IdleDroneGuard } from './guards/idle-drone.guard';
+import { DroneBatteryGuard } from './guards/drone-battery.guard';
 
+@ApiTags('Dispatch')
 @Controller('dispatch')
 export class DispatchController {
   constructor(private readonly dispatchService: DispatchService) {}
